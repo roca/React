ae68ceeb3b405edc3ebd1d3b54ab8268f56f6a56
Romel Campbell <romel.campbell@regeneron.com>
2015-12-21 14:34:52 -0500

    Made my firstr React Component

---
+++ pluralsight/ReactOnRails/twitter-clone/app/assets/javascripts/components.jsx
@@ -0,0 +1,20 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <h1> Important content for SEO here... </h1>
+    );
+  }
+}
+
+
+
+
+// let documentReady = () => {
+//    React.render (
+//      <Main/>,
+//        document.getElementById('react')
+//    );
+// };
+//
+//
+// $(documentReady);
