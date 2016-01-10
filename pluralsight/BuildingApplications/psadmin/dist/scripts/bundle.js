1c95ad3154209e705a50ea9ca49ee6aef6ddb873
Romel Campbell <romel.campbell@regeneron.com>
2016-01-10 05:54:11 -0500

    Create a route logic

--- pluralsight/BuildingApplications/psadmin/dist/scripts/bundle.js
+++ pluralsight/BuildingApplications/psadmin/dist/scripts/bundle.js
@@ -29026,6 +29026,38 @@ module.exports = require('./lib/React');
 
 var React = require('react');
 
+var About = React.createClass({displayName: "About",
+    render: function() {
+      return (
+        React.createElement("div", null, 
+          React.createElement("h1", null, "About"), 
+          React.createElement("p", null, 
+            "This application uses the following technologies:", 
+
+              React.createElement("ul", null, 
+                React.createElement("li", null, "React"), 
+                React.createElement("li", null, "React Router"), 
+                React.createElement("li", null, "Flux"), 
+                React.createElement("li", null, "Node"), 
+                React.createElement("li", null, "Gulp"), 
+                React.createElement("li", null, "Browserify"), 
+                React.createElement("li", null, "BBoostrap")
+              )
+
+          )
+        )
+      );
+    }
+});
+
+
+module.exports = About;
+
+},{"react":157}],159:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+
 var Home = React.createClass({displayName: "Home",
 
   render: function() {
@@ -29041,13 +29073,43 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":157}],159:[function(require,module,exports){
-"use strict";
-
-var jQuery = require('jquery');
+},{"react":157}],160:[function(require,module,exports){
+$ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-React.render(React.createElement(Home, null), document.getElementById('app'));
+(function(win, doc) {
+"use strict";
 
-},{"./components/homePage":158,"jquery":2,"react":157}]},{},[159]);
+   var App = React.createClass({displayName: "App",
+     render: function() {
+       var Child;
+
+       switch(this.props.route) {
+         case 'about': Child = About; break;
+         default: Child = Home;
+       }
+
+       return (
+          React.createElement("div", null, 
+            React.createElement(Child, null)
+          )
+       );
+     }
+   });
+
+  function render() {
+
+    var route = win.location.hash.substr(1);
+    React.render(React.createElement(App, {route: route}), doc.getElementById('app'));
+
+   }
+
+   win.addEventListener('hashchange', render);
+   render();
+
+
+})(window, document);
+
+},{"./components/about/aboutPage":158,"./components/homePage":159,"jquery":2,"react":157}]},{},[160]);
