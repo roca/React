0ef2b4c28e0f48354a1b8afa3a1134c01ce22ae4
Romel Campbell <romel.campbell@regeneron.com>
2016-01-16 06:09:14 -0500

    Finished Module on React Router 7

--- pluralsight/BuildingApplications/psadmin/dist/scripts/bundle.js
+++ pluralsight/BuildingApplications/psadmin/dist/scripts/bundle.js
@@ -46461,6 +46461,21 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+    statics: {
+      willTransitionTo: function(transition, params, query, callback) {
+        if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+          transition.about();
+        } else {
+          callback();
+        }
+      },
+      willTransitionFrom: function(transition, component) {
+        if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+            transition.about();
+        }
+      }
+
+    },
     render: function() {
       return (
         React.createElement("div", null, 
