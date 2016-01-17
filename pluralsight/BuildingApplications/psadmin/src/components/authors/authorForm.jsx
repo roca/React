2e655ab8d5befa7957e6073d582ffd2e1c6cb063
Romel Campbell <romel.campbell@regeneron.com>
2016-01-17 06:12:15 -0500

    Implemented form validation

--- pluralsight/BuildingApplications/psadmin/src/components/authors/authorForm.jsx
+++ pluralsight/BuildingApplications/psadmin/src/components/authors/authorForm.jsx
@@ -14,12 +14,14 @@ var AuthorForm = React.createClass({
           name="firstName"
           label="First Name"
           onChange={this.props.onChange}
+          error={this.props.errors.firstName}
           value={this.props.author.firstName}/>
 
         <Input
           name="lastName"
           label="Last Name"
           onChange={this.props.onChange}
+          error={this.props.errors.lastName}
           value={this.props.author.lastName}/>
 
         <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave}/>
--- pluralsight/BuildingApplications/psadmin/src/components/authors/manageAuthorPage.jsx
+++ pluralsight/BuildingApplications/psadmin/src/components/authors/manageAuthorPage.jsx
@@ -1,14 +1,20 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm.jsx');
 var AuthorApi = require('../../api/authorApi.js');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
+  mixins: [
+    Router.Navigation
+  ],
 
   getInitialState: function() {
     return {
-      author: {id: '', firstName: '', lastName: '' }
+      author: {id: '', firstName: '', lastName: '' },
+      errors: {}
     };
   },
 
@@ -18,10 +24,32 @@ var ManageAuthorPage = React.createClass({
     this.state.author[field] = value;
     return this.setState({author: this.state.author});
   },
+  authorFormIsValid: function() {
+    var formIsValid = true;
+    this.state.errors = {};
+     if (this.state.author.firstName.length < 3) {
+       this.state.errors.firstName = "First name must be at least 3 characters";
+       formIsValid = false;
+     }
+     if (this.state.author.lastName.length < 3) {
+       this.state.errors.lastName = "Last name must be at least 3 characters";
+       formIsValid = false;
+     }
+
+     this.setState({errors: this.state.errors});
+
+     return formIsValid;
+  },
 
   saveAuthor: function(event) {
     event.preventDefault();
+
+    if (!this.authorFormIsValid()) {
+      return;
+    }
     AuthorApi.saveAuthor(this.state.author);
+    toastr.success('Author saved.');
+    this.transitionTo('authors');
   },
 
   render: function() {
@@ -29,6 +57,7 @@ var ManageAuthorPage = React.createClass({
         <AuthorForm
           author={this.state.author}
           onChange={this.setAuthorState}
+          errors={this.state.errors}
           onSave={this.saveAuthor} />
     );
   }
