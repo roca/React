0e0f7535cb949b97e27a5f896c913e1f1ddac3db
roca <RomelCampbell@gmail.com>
2016-01-23 06:09:37 -0500

    Building List with Map

---
+++ udemy/ReduxSimpleStarter/src/components/video_list.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+
+	 const videItems = props.videos.map((video) => {
+	 	return <VideoListItem video={video} />
+	 });
+
+	return (
+		<ul className="col-sd-4 list-group">
+			{ videItems }
+		</ul>
+	)
+}
+
+
+export default VideoList;
--- udemy/ReduxSimpleStarter/src/index.jsx
+++ udemy/ReduxSimpleStarter/src/index.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyA_q9hMVS0wi8nIjz1o2BmxfA7_wGL4u3k";
 
 
-const App = () => {
-  return (
-  		<div>
-  			<SearchBar />
-  		</div>
-  )
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+		
+		YTSearch({key: API_KEY, term: 'surfboards'},  (videos)  => {
+			this.setState({ videos });
+		});
+
+
+	}
+
+  	render() {
+		return (
+				<div>
+					<SearchBar />
+					<VideoList videos={ this.state.videos } />
+				</div>
+		)
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
