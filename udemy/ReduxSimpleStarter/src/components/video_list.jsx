35470dd2f061540cf81ae8f5ffe7e53eb7c486b1
roca <RomelCampbell@gmail.com>
2016-01-24 06:17:07 -0500

    Lecture 29 Video Selection

--- udemy/ReduxSimpleStarter/src/components/video_list.jsx
+++ udemy/ReduxSimpleStarter/src/components/video_list.jsx
@@ -5,15 +5,19 @@ import VideoListItem from './video_list_item';
 const VideoList = (props) => {
 
 	 const videItems = props.videos.map((video) => {
-	 	return <VideoListItem key={video.etag} video={video} />
+	 	return (
+	 		<VideoListItem 
+	 			onVideoSelect = {props.onVideoSelect}
+	 			key={video.etag} video={video} />
+	 	);
 	 });
 
 	return (
 		<ul className="col-sd-4 list-group">
 			{ videItems }
 		</ul>
-	)
-}
+	);
+};
 
 
 export default VideoList;
--- udemy/ReduxSimpleStarter/src/index.jsx
+++ udemy/ReduxSimpleStarter/src/index.jsx
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyA_q9hMVS0wi8nIjz1o2BmxfA7_wGL4u3k";
 
@@ -13,10 +14,16 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
-		
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+		};
+
 		YTSearch({key: API_KEY, term: 'surfboards'},  (videos)  => {
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos ,
+				selectedVideo: videos[0]
+			});
 		});
 
 
@@ -26,7 +33,10 @@ class App extends Component {
 		return (
 				<div>
 					<SearchBar />
-					<VideoList videos={ this.state.videos } />
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList 
+						onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+						videos={ this.state.videos } />
 				</div>
 		)
 	}
