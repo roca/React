de371cfbae132500ec5526620468aead3cc0ec97
Romel Campbell <romel.campbell@regeneron.com>
2016-01-25 16:40:13 -0500

    L 42 Containers and Reducers

--- udemy/ReactRedux/TenderApp/src/components/app.js
+++ udemy/ReactRedux/TenderApp/src/components/app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Component } from 'react';
 
+
+import BookList from '../containers/book-list';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+
+           <BookList />
+
+      </div>
     );
   }
 }
