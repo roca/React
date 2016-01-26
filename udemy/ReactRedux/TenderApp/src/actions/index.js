5b3b916b1955b581b32103ac9b27a613d724e0f4
roca <RomelCampbell@gmail.com>
2016-01-26 04:09:51 -0500

    Created a action creator

---
+++ udemy/ReactRedux/TenderApp/src/actions/index.js
@@ -0,0 +1,3 @@
+export function selectBoook (argument) {
+	console.log('A book has been selected', book.title);
+}
--- udemy/ReactRedux/TenderApp/src/containers/book-list.jsx
+++ udemy/ReactRedux/TenderApp/src/containers/book-list.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { selectBook } from '../actions/index';
+
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
@@ -26,5 +28,9 @@ function mapStateToProps(state) {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
 
-export default connect(mapStateToProps)(BookList)
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
