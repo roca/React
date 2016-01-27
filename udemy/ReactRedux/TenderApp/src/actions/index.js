b2e0a346f37413c4b87e6f093a04a7fdd1156266
roca <RomelCampbell@gmail.com>
2016-01-26 21:28:38 -0500

    L45

--- udemy/ReactRedux/TenderApp/src/actions/index.js
+++ udemy/ReactRedux/TenderApp/src/actions/index.js
@@ -1,3 +1,6 @@
-export function selectBoook (book) {
-	console.log('A book has been selected', book.title);
+export function selectBook (book) {
+	return {
+		type: 'BOOK_SELECTED',
+		payload: book
+	};
 }
