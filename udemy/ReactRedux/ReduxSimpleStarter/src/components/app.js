973bfeb7cad9cead435dd50bfac71e7df22de009
Romel Campbell <romel.campbell@regeneron.com>
2016-01-30 19:55:58 -0500

    Saving the simple starter

--- udemy/ReactRedux/ReduxSimpleStarter/src/components/app.js
+++ udemy/ReactRedux/ReduxSimpleStarter/src/components/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
 
-//ALL requests require a key parameter, eg `reduxblog.herokuapp.com/api/posts?key=Z3vLJf$jCNxH3h`
-
 export default class App extends Component {
   render() {
     return (
