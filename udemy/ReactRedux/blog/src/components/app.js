349869742cbe733f7e97eccd695a44a9cb1846a4
Romel Campbell <romel.campbell@regeneron.com>
2016-01-31 06:57:44 -0500

    Wired up actions L80

--- udemy/ReactRedux/blog/src/components/app.js
+++ udemy/ReactRedux/blog/src/components/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Component } from 'react';
 
-//ALL requests require a key parameter, eg `reduxblog.herokuapp.com/api/posts?key=Z3vLJf$jCNxH3h`
-
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
