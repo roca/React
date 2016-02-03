99a098591f3dc188fb60feeccc32b1cdcfe621dc
Romel Campbell <romel.campbell@regeneron.com>
2016-02-03 09:32:47 -0500

    Reimplementaion of creatStore

--- EggHead/redux/index.js
+++ EggHead/redux/index.js
@@ -1,5 +1,4 @@
-import expect from 'expect';
-import { createStore } from 'redux';
+//import { createStore } from 'redux';
 
 const  counter  = (state = 0, action)  => {
 
@@ -11,6 +10,30 @@ const  counter  = (state = 0, action)  => {
 		default:
 			return state;
 	}
+}
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state,action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter( l => l !== listener);
+      };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe }
 }
 
 const store = createStore(counter);
