7b4bebc17802388a61cccbcb776ddd7f2703c747
Romel Campbell <romel.campbell@regeneron.com>
2016-02-03 21:02:40 -0500

    L8

--- EggHead/redux/bundle.js
+++ EggHead/redux/bundle.js
@@ -44,71 +44,8 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"./index.js\""); }());
 
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	'use strict';
-
-	//import { createStore } from 'redux';
-
-	var counter = function counter() {
-	  var state = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
-	  var action = arguments[1];
-
-	  switch (action.type) {
-	    case 'INCREMENT':
-	      return state + 1;
-	    case 'DECREMENT':
-	      return state - 1;
-	    default:
-	      return state;
-	  }
-	};
-
-	var createStore = function createStore(reducer) {
-	  var state = undefined;
-	  var listeners = [];
-
-	  var getState = function getState() {
-	    return state;
-	  };
-
-	  var dispatch = function dispatch(action) {
-	    state = reducer(state, action);
-	    listeners.forEach(function (listener) {
-	      return listener();
-	    });
-	  };
-
-	  var subscribe = function subscribe(listener) {
-	    listeners.push(listener);
-	    return function () {
-	      listeners = listeners.filter(function (l) {
-	        return l !== listener;
-	      });
-	    };
-	  };
-
-	  dispatch({});
-
-	  return { getState: getState, dispatch: dispatch, subscribe: subscribe };
-	};
-
-	var store = createStore(counter);
-	var render = function render() {
-	  document.body.innerText = store.getState();
-	};
-
-	store.subscribe(render);
-	document.addEventListener("DOMContentLoaded", render);
-
-	document.addEventListener('click', function () {
-	  store.dispatch({ type: 'INCREMENT' });
-	});
 
 /***/ }
 /******/ ]);
--- EggHead/redux/webpack.config.js
+++ EggHead/redux/webpack.config.js
@@ -10,10 +10,7 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-            presets: ['es2015']
-          }
+      loader: 'babel'
     }]
   },
   resolve: {
