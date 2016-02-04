46a62c1ed517228ba8147d12841176988f94bbc3
Romel Campbell <romel.campbell@regeneron.com>
2016-02-04 16:43:22 -0500

    L10 toggle todo

--- EggHead/redux/index.js
+++ EggHead/redux/index.js
@@ -1,56 +1,28 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-const addCounter = (list) => {
-  return [...list,0];
-};
+const toggleTodo = (todo) => {
+ return {...todo,completed: !todo.completed};
+}
 
-const removeCounter = (list, index) => {
-   return [
-     ...list.slice(0, index),
-     ...list.slice(index + 1)
-   ];
-};
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
 
-const incrementCounter = (list, index) => {
-   return [
-     ...list.slice(0, index),
-     (list[index] + 1) ,
-     ...list.slice(index + 1)
-   ]
-};
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
 
+  deepFreeze(todoBefore);
 
-const testAddCounter = () => {
-  const listBefore = [];
-  const listAfter = [0];
-
-  deepFreeze(listBefore);
-
-  expect(addCounter(listBefore)).toEqual(listAfter);
-};
-
-const testRemoveCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter = [0, 20];
-
-  deepFreeze(listBefore);
-
-  expect(removeCounter(listBefore, 1)).toEqual(listAfter);
-
-};
-
-const testIncrementCounter = () => {
-  const listBefore = [0, 10, 20];
-  const listAfter = [0, 11,  20];
-
-  deepFreeze(listBefore);
-
-  expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+  expect(toggleTodo(todoBefore)).toEqual(todoAfter);
 
 }
 
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-console.log('All tests passed.');
+testToggleTodo();
+console.log('All tests passed');
