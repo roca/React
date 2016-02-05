3adc7f15173db51079701d48fd133b6d3cf49270
roca <RomelCampbell@gmail.com>
2016-02-05 05:37:52 -0500

    Lgit add . todo_list_reducer

--- EggHead/redux/index.js
+++ EggHead/redux/index.js
@@ -1,28 +1,4 @@
-import expect from 'expect';
-import deepFreeze from 'deep-freeze';
+import {testAddTodo as testL11} from './todo_list_reducer'; //Test from L11
 
-const toggleTodo = (todo) => {
- return {...todo,completed: !todo.completed};
-}
 
-const testToggleTodo = () => {
-  const todoBefore = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: false
-  };
-
-  const todoAfter = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: true
-  };
-
-  deepFreeze(todoBefore);
-
-  expect(toggleTodo(todoBefore)).toEqual(todoAfter);
-
-}
-
-testToggleTodo();
-console.log('All tests passed');
+testL11(); //Test from L11
