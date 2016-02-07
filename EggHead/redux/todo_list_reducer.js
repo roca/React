b01e80e94ffb9d972be8b4e2314133caa78f1b26
roca <RomelCampbell@gmail.com>
2016-02-07 05:48:13 -0500

    L16

--- EggHead/redux/todo_list_reducer.js
+++ EggHead/redux/todo_list_reducer.js
@@ -1,46 +1,9 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 import { createStore } from 'redux';
+//import { combineReducers } from 'redux';
+import { todo, todos } from './actions/index';
 
-
-const todo = (state, action) => {
-	switch (action.type) {
-		case 'ADD_TODO':
-		return {
-							id: action.id,
-							text: action.text,
-							completed: false
-						};
-		case 'TOGGLE_TODO':
-				if (state.id !== action.id) {
-					return state;
-				}
-				return {...state,completed: !state.completed}
-			default:
-			return state;
-	}
-
-}
-
-const todos = (state=[],action) => {
-	switch (action.type) {
-		case 'ADD_TODO':
-		 return [
-		 ...state,
-			todo(undefined,action)
-		 ];
-	  case 'TOGGLE_TODO':
-		// return [
-    //   ...state.slice(0, action.id),
-    //   {...state[action.id],completed: !state[action.id].completed} ,
-    //   ...state.slice(action.id + 1)
-    // ]
-		return state.map(t => todo(t, action));
-		default:
-		 return state;
-	}
-	return state
-}
 
 export const testAddTodo = () => {
 	const stateBefore = [];
@@ -111,12 +74,34 @@ export const testToggleTodo = () => {
 		}
 	}
 
-	const todoApp = (state= {}, action) => {
-		return {
-			todos: todos(state.todos,action),
-			visibilityFilter: visibilityFilter(state.visibilityFilter,action)
-		};
-	}
+
+  const combineReducers = (reducers) => {
+  	return (state={},action) => {
+  		return Object.keys(reducers).reduce(
+  			(nextState,key) => {
+  				nextState[key] = reducers[key](
+  					state[key],
+  					action
+  				);
+  				return nextState;
+  			},
+  			{}
+  		);
+  	};
+  };
+
+
+   const todoApp = combineReducers ({
+   	todos,
+   	visibilityFilter
+   });
+ 
+	// const todoApp = (state= {}, action) => {
+	// 	return {
+	// 		todos: todos(state.todos,action),
+	// 		visibilityFilter: visibilityFilter(state.visibilityFilter,action)
+	// 	};
+	// }
 
 
 	const store = createStore(todoApp);
