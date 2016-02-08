41433af393b115cd8424ef5223967a81a7407213
Romel Campbell <romel.campbell@regeneron.com>
2016-02-08 09:10:50 -0500

    L18

--- EggHead/redux/index.js
+++ EggHead/redux/index.js
@@ -20,8 +20,15 @@ class TodoApp extends Component {
 					this.input.value = '';
 				}}>Add todo</button>
 				<ul>
-					{ this.props.todos.map(todo => 
-						<li key={todo.id}>
+					{ this.props.todos.map(todo =>
+						<li key={todo.id} onClick={() =>{
+							store.dispatch({
+								type: 'TOGGLE_TODO',
+								id: todo.id
+							});
+						}} style = {{
+							textDecoration: todo.completed ? 'line-through' : 'none'
+						}}>
 							{todo.text}
 						</li>
 					)}
