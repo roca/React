37c1a86bc0db88c87f94ec9b941882f0fae9e418
Romel Campbell <romel.campbell@regeneron.com>
2016-03-20 06:13:53 -0400

    WIP

---
+++ pluralsight/ReactNative/PluralTodo/main.js
@@ -0,0 +1,23 @@
+const React = require('react-native');
+const {
+  AppRegistry,
+  Component,
+  Text,
+} = React;
+
+class PluralTodo extends Component {
+  constructor(props, context) {
+      super(props, context);
+      this.state = {
+      };
+  }
+
+  render() {
+      return (
+        <Text>Hello there!</Text>
+      );
+  }
+
+}
+
+AppRegistry.registerComponent('main', () => PluralTodo);
