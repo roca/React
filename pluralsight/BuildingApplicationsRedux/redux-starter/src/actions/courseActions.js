05051fd26faaa7fd5db4d48c0fbdd064674ffd91
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-08-27 06:02:08 -0400

    Creates Course List in a table

--- pluralsight/BuildingApplicationsRedux/redux-starter/src/actions/courseActions.js
+++ pluralsight/BuildingApplicationsRedux/redux-starter/src/actions/courseActions.js
@@ -1,8 +1,8 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course};
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
 export function loadCourses() {
