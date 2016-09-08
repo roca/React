8f5c7e42e93b8bbe0f78916e42406134f0e855c1
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-09-08 05:22:28 -0400

    Wrote tests usingi TestUtils

--- pluralsight/BuildingApplicationsRedux/redux-starter/src/components/course/CourseForm.test.js
+++ pluralsight/BuildingApplicationsRedux/redux-starter/src/components/course/CourseForm.test.js
@@ -3,9 +3,9 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup() {
+function setup(saving) {
   let props = {
-    course: {}, saving: false, errors: {},
+    course: {}, saving: saving, errors: {},
     onSave: () => {},
     onChange: () => {}
   };
@@ -26,5 +26,21 @@ describe('CourseForm via React Test Utils', () => {
   it('renders form and h1', () => {
     const { output } = setup();
     expect(output.type).toBe('form');
+    let [ h1 ] = output.props.children;
+    expect(h1.type).toBe('h1');
   });
+
+
+  it('save button is labled "Save" when not saving', () => {
+    const { output } = setup(false);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Save');
+  });
+
+  it('save button is labled "Saving..." when saving', () => {
+    const { output } = setup(true);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Saving...');
+  });
+
 });
