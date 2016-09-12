a62431c498d405c65b3256c6bc12375e67e98c38
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-09-12 04:21:43 -0400

    Testing Action Creators

--- pluralsight/BuildingApplicationsRedux/redux-starter/src/components/course/ManageCoursePage.test.js
+++ pluralsight/BuildingApplicationsRedux/redux-starter/src/components/course/ManageCoursePage.test.js
@@ -8,12 +8,14 @@ describe  ("Manage Course Page", () => {
   it("sets  error message when trying to save empty title", () => {
     const props = {
       authors: [],
+      actions: { saveCourse: () => Promise.resolve()},
       course: {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''}
     };
-    
+
     const wrapper = mount(<ManageCoursePage {...props}/>);
     const saveButton = wrapper.find('input').last();
     expect(saveButton.prop('type')).toBe('submit');
     saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
   });
 });
