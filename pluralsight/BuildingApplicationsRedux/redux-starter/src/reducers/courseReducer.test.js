53fda929e9465ac686e371cc5b68a17989bddf98
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2016-09-14 04:28:47 -0400

    Test for Reducers

--- pluralsight/BuildingApplicationsRedux/redux-starter/src/reducers/courseReducer.test.js
+++ pluralsight/BuildingApplicationsRedux/redux-starter/src/reducers/courseReducer.test.js
@@ -19,4 +19,26 @@ describe('Course Reducer', () => {
     expect(newState.length).toEqual(3);
   });
 
+  it('should update course when passed UPDATE_COURSE_SUCCESS', function() {
+   // arange
+    const initialSate = [
+      {id: 'A', title: 'A'},
+      {id: 'B', title: 'B'},
+      {id: 'C', title: 'C'}
+    ];
+    const course = {id: 'B', title: 'New Title'};
+    const action = actions.updateCourseSuccess(course);
+
+    //acct
+    const newState = courseReducer(initialSate, action);
+    const updatedCourse = newState.find( a => a.id === course.id);
+    const untouchedCourse = newState.find( a => a.id === 'A');
+
+    //assert
+    expect(updatedCourse.title).toEqual('New Title');
+    expect(untouchedCourse.title).toEqual('A');
+    expect(newState.length).toEqual(3);
+  });
+
+
 });
