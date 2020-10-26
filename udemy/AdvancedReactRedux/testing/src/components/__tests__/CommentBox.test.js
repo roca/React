6dabfcabea1cba766521b63d2a32f50366a6771f
roca <romel.campbell@regeneron.com>
2020-10-26 12:39:13 -0400

    10262020/12:37 Encapsulated 2 tests in a describe block

--- udemy/AdvancedReactRedux/testing/src/components/__tests__/CommentBox.test.js
+++ udemy/AdvancedReactRedux/testing/src/components/__tests__/CommentBox.test.js
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 
-let wrapped;
+let wrapped, mockEvent;
 
 beforeEach(() => {
     wrapped = mount(<CommentBox />);
@@ -16,4 +16,25 @@ afterEach(() => {
 it('should have a textarea and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('The textarea ', () =>{
+
+    beforeEach(() => {
+        mockEvent = { target: { value: "new comment" } };
+        wrapped.find('textarea').simulate('change', mockEvent);
+        wrapped.update();
+    });
+    
+
+    it("should allow users to type in text", () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual(mockEvent.target.value);
+    });
+
+    it("should be empty when the form is submitted ", () => {
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+
 });
