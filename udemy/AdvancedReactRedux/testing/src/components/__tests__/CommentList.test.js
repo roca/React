d452658d2807968b485b7c0d2a92137826deeaad
roca <romel.campbell@regeneron.com>
2020-11-02 13:54:37 -0500

    11022020/13:54

---
+++ udemy/AdvancedReactRedux/testing/src/components/__tests__/CommentList.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import cheerio from 'cheerio';
+
+import Root from 'Root';
+
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+    const initialState = {
+        comments: ['Comment 1', 'Comment 2']
+    };
+
+    wrapped = mount(
+        <Root initialState={initialState}>
+            <CommentList />
+        </Root>
+    );
+});
+
+afterEach(() => {
+    wrapped.unmount();
+});
+
+it('should create on LI per comment',() => {
+    expect(wrapped.find('li').length).toEqual(2);
+});
+
+it('should show the text for each comment', () => {
+ const $ = cheerio.load(wrapped.render().html());  
+
+ expect($('li').first().text()).toContain('Comment 1');
+ expect($('li:nth-child(2)').text()).toContain('Comment 2');
+});
