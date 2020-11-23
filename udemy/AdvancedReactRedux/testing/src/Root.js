5b02e5b337d6d798baa2ff951f3f66b1a2732847
roca <romel.campbell@regeneron.com>
2020-11-23 16:17:55 -0500

    Finished section 4 on Middlewares

--- udemy/AdvancedReactRedux/testing/src/Root.js
+++ udemy/AdvancedReactRedux/testing/src/Root.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 //import reduxPromise from "redux-promise";
+
 import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
 import { createStore, applyMiddleware } from "redux";
 
 import reducers from "reducers";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(reducers, initialState, applyMiddleware(async, stateValidator));
   return <Provider store={store}>{children}</Provider>;
 };
