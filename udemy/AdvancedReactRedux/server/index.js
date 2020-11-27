fca429f0cc309c40936bdd87c7e8a8c139a7a695
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-11-27 15:24:39 -0500

    refactor: mongo crud operation for User model working now

--- udemy/AdvancedReactRedux/server/index.js
+++ udemy/AdvancedReactRedux/server/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const router = require('./router');
+
+// DB Setup
+mongoose.connect('mongodb://mongo:27017/auth');
 
 // App setup
 const app = express();
