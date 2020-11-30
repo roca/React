9eee3f7c01b0f23ae84b73fa3ba2cfff03ec9498
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2020-11-30 05:34:55 -0500

    refactor: user signup, signin and auth token using password strategies

--- udemy/AdvancedReactRedux/server/services/passport.js
+++ udemy/AdvancedReactRedux/server/services/passport.js
@@ -2,16 +2,36 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
 const User = require('../models/user');
 const config = require('../secret/config');
+
+// Setup options for Local Strategy
+const localOptions = {usernameField: 'email'};
+// Create local Strategy
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    // Verify this username and password, call done with the user
+    // if it is correct otherwise, call done with false
+    User.findOne({email: email}, function(err, user) {
+        if (err) { return done(err, false); }
+        if (!user) { return done(null, false); }
+
+        // Compare passwords - is 'password' equal to user.password?
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) { return done(err, false); }
+            if (!isMatch) { return done(null, false); }   
+            
+            return done(null, user);
+        });
+    });
+});
 
 // Setup options for JWT Strategy
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret
 };
-
 // Create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     // See if the user ID in the payload exists in our database
@@ -30,3 +50,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
