3274008cf45ef16797d2f8868db8ac936667d26a
roca <romel.campbell@regeneron.com>
2020-12-02 11:09:16 -0500

    fix: add commitlint config

---
+++ udemy/AdvancedReactRedux/client/release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/roca/React",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
+};
---
+++ udemy/AdvancedReactRedux/client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Header from "./Header";
+
+export default () => {
+  return (
+    <div>
+      <Header />
+      Hi, im the app
+    </div>
+  );
+};
