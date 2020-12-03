33c0315fc44c1eccd9bfa0a13f2043801827d4d5
roca <romel.campbell@regeneron.com>
2020-12-03 15:49:34 -0500

    refactor: setup reducers

--- udemy/AdvancedReactRedux/client/src/index.js
+++ udemy/AdvancedReactRedux/client/src/index.js
@@ -1,22 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
-import { createBrowserHistory } from "history";
+import reducers from './reducers';
 
+import { createBrowserHistory } from "history";
 export const history = createBrowserHistory({
   basename: process.env.PUBLIC_URL,
 });
 
 ReactDOM.render(
-  <BrowserRouter basename="/dev">
-    <App>
-      <Route path="/" exact component={Welcome} />
-      <Route path="/signup" exact component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter basename="/dev">
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
---
+++ udemy/AdvancedReactRedux/client/src/reducers/auth.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+const INITIAL_STATE = {
+    authenticated: '',
+    errorMessage: ''
+};
+
+export default function(state = INITIAL_STATE, action) {
+    return state;
+}
